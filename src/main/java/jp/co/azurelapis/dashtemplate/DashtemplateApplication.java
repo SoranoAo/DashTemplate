3f9a45ee53762a58292facfd5ed0a2075aefd237
Sotano Ao <sora.aoi.clover@gmail.com>
2018-08-05 15:36:27 +0900

    プロジェクトテンプレート追加

---
+++ src/main/java/jp/co/azurelapis/dashtemplate/DashtemplateApplication.java
@@ -0,0 +1,12 @@
+package jp.co.azurelapis.dashtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashtemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashtemplateApplication.class, args);
+	}
+}
